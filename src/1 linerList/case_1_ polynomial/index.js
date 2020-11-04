2ba3a35279e6d9d5aa5fd8efa06d2c13e1c5f40a
lizuoxing <lizuoxing@xiaomi.com>
2020-11-04 21:49:16 +0800

    create: polynomialPlus

---
+++ src/1 linerList/case_1_ polynomial/index.js
@@ -0,0 +1,17 @@
+var polynomial1 = [
+  [2, 1],
+  [5, 3],
+  [-7, 4],
+  [6, 7],
+];
+
+var polynomial2 = [
+  [3, 0],
+  [4, 3],
+  [2, 5],
+  [3, 10],
+];
+
+function polynomialPlus(polynomial1, polynomial2) {
+
+}
